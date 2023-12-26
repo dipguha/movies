52a2c6cd8a7be0019c2ad4e466ff3ba39aa8ef50
Chandradip Guha <chandradip.work@gmail.com>
2023-12-26 12:15:15 +0000

    added movie and useParams

--- go-movies-front-end/src/components/Movie.js
+++ go-movies-front-end/src/components/Movie.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 const Movie = () => {
+
+    //use empty object as an initial value
+    const[movie, setMovie] = useState({});
+    //useParams comes from react-router
+    let { id } = useParams();
+
+    useEffect( () => {
+        let myMovie = { id: 1, title: "Movie 1", release_date: "01-01-2001", runtime: 111, mpaa_rating: "R1", description: "Description 1" }
+        setMovie(myMovie)
+    }, [id] )
 
     return (
         <div>
-            <h2>Movie screen</h2>
+            <h2>Movie: {movie.title}</h2>
+            <small><em>{movie.release_date}, {movie.runtime} minutes, Rated {movie.mpaa_rating}</em></small>
             <hr />
+            <p>{movie.description}</p>
         </div>
     )
 }
--- go-movies-front-end/src/index.js
+++ go-movies-front-end/src/index.js
@@ -6,6 +6,7 @@ import ErrorPage from './components/ErrorPage';
 
 import Home from './components/Home';
 import Movies from './components/Movies';
+import Movie from './components/Movie';
 import Genres from './components/Genres';
 import EditMovie from './components/EditMovie';
 import ManageCatalogue from './components/ManageCatalogue';
@@ -21,6 +22,7 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: "/movies", element: <Movies /> },
+      { path: "/movies/:id", element: <Movie /> },
       { path: "/genres", element: <Genres /> },
       { path: "/admin/movie/0", element: <EditMovie /> },
       { path: "/manage-catalogue", element: <ManageCatalogue /> },
