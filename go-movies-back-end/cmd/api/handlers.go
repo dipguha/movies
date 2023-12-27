9be421545a020ccb802f1e4d5223522f6f7186bd
Chandradip Guha <chandradip.work@gmail.com>
2023-12-27 16:22:17 +0000

    added movies json data

--- go-movies-back-end/cmd/api/handlers.go
+++ go-movies-back-end/cmd/api/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/internal/models"
 	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -27,4 +29,47 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(out)
 
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "2001-01-01")
+
+	movie1 := models.Movie{
+		ID:          1,
+		Title:       "Movie 1",
+		ReleaseDate: rd,
+		RunTime:     111,
+		MPAARating:  "R1",
+		Description: "Description 1",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, movie1)
+
+	rd, _ = time.Parse("2006-01-02", "2002-02-02")
+
+	movie2 := models.Movie{
+		ID:          2,
+		Title:       "Movie 2",
+		ReleaseDate: rd,
+		RunTime:     222,
+		MPAARating:  "R2",
+		Description: "Description 2",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, movie2)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		log.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
